cc897de86213c5e46cc34bb246bce143257123dc
andredvlsk <dehdovalski@gmail.com>
2023-11-26 17:17:58 +0100

    sql update

--- SQL/ejercicio3.sql
+++ SQL/ejercicio3.sql
@@ -9,3 +9,39 @@ primaria), "nombre" (texto) y "precio" (numérico).
 nombres de los productos que han comprado (utiliza un INNER JOIN con la
 tabla "Productos").
 */
+
+CREATE TABLE IF NOT EXISTS productos(
+	id SERIAL PRIMARY KEY,
+	nombre VARCHAR(255),
+	precio NUMERIC
+)
+
+INSERT INTO public.productos(nombre, precio)
+VALUES
+	('Patata', 2.10),
+	('Aceite', 7.30),
+	('Cebolla', 1.20),
+	('Té', 0.80),
+	('Café', 3.15);
+
+UPDATE public.productos
+SET precio = 4.22
+WHERE nombre = 'Café'
+
+DELETE from public.productos
+WHERE nombre = 'Té'
+
+ALTER TABLE public.usuarios
+ADD producto_comprado INT
+
+UPDATE public.usuarios
+SET producto_comprado = 2
+WHERE id = 1;
+
+ALTER TABLE public.usuarios
+ADD CONSTRAINT FK_producto_comprado FOREIGN KEY (producto_comprado) REFERENCES productos(id);
+
+SELECT usuarios.nombre, productos.nombre
+FROM public.productos
+INNER JOIN public.usuarios
+ON usuarios.producto_comprado = productos.id;
--- SQL/ejercicio4.sql
+++ SQL/ejercicio4.sql
@@ -15,3 +15,48 @@ realizado un pedido, pero también los que no han realizado ningún pedido
 5. Agrega una nueva columna llamada "cantidad" a la tabla "Pedidos" y actualiza
 los registros existentes con un valor (utiliza ALTER TABLE y UPDATE)
 */
+
+CREATE TABLE pedidos(
+	id INT PRIMARY KEY,
+	id_usuario INT ,
+	id_producto INT,
+	FOREIGN KEY (id_usuario) REFERENCES usuarios(id),
+	FOREIGN KEY (id_producto) REFERENCES productos(id)
+)
+
+INSERT INTO pedidos (id, id_usuario, id_producto)
+VALUES 
+	(1, 3, 2),
+	(2, 1, 5),
+	(3, 1, 3),
+	(4, 1, 1);
+
+SELECT usuarios.nombre, coalesce(productos.nombre,'NA')
+FROM public.usuarios
+LEFT JOIN public.productos
+ON usuarios.producto_comprado = productos.id
+
+SELECT usuarios.nombre
+FROM public.usuarios
+LEFT JOIN public.pedidos
+ON usuarios.id = pedidos.id_usuario
+GROUP BY usuarios.nombre
+
+ALTER TABLE public.pedidos
+ADD cantidad NUMERIC;
+
+UPDATE public.pedidos
+SET cantidad = 1
+WHERE id = 1;
+
+UPDATE public.pedidos
+SET cantidad = 10
+WHERE id = 2;
+
+UPDATE public.pedidos
+SET cantidad = 3.5
+WHERE id = 3;
+
+UPDATE public.pedidos
+SET cantidad = 5
+WHERE id = 4;
